cf9ac92db765837d6f3927883cd0209516b24b30
tari2025 <denilestari272@gmail.com>
2025-05-08 21:00:34 +0700

    coding node

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -87,6 +87,95 @@ public:
             START = START->next;
         else
             previous->next = current->next;
+
+        delete current;
+        return true;
     }
+
+    void traverse()
+} if (listEmpty())
+{
+    cout << "\nList Kosong\n";
+}
+else
+{
+    cout << "\nData didalam list adalah:\n";
+    Node *currentNode = START;
+    while (currentNode != NULL)
+    {
+        cout << currentNode->noMhs << endl;
+        currentNode = currentNode->next;
+    }
+    cout << endl;
 }
 }
+}
+;
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menambahkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl;
+        << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+        swith(ch)
+        {
+        case '1':
+        {
+            mhs.addNote();
+            break;
+        }
+
+        case '2':
+        {
+            if (mhs.listEmpty())
+            {
+                cout << endl
+                     << list Kosong " << endl;
+                    break;
+            }
+            cout << endl
+                 << " \nMasukkan no mahasiswa yang akan dihapus : ";
+            cin >> nim;
+            if (mhs.delNode(nim) == false)
+                cout << endl
+                     << data tidak ditemukan " << endl;
+                    else cout
+                     << endl;
+            << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
+        }
+        break;
+        case '3':
+        {
+            mhs.traverse();
+        }
+        break;
+
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << endl
+                 << "Masukkan no mahasiswa yang dicari : ";
+            cin >> nim;
+            if (mhs.Seacrh(nim.& previous.& current) == false)
+                cout << endl
+                     << "Data tidak ditemukan << endl;
+                    else "
+        }
+        }
