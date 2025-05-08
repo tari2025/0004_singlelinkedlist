f06d5c35769e274d11d9f50b4069f66dff527d3c
tari2025 <denilestari272@gmail.com>
2025-05-08 20:06:38 +0700

    node

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
+
 class Node
 {
 public:
     int noMhs;
     Node *next;
 };
+
 class LinkedList
 {
-}
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mhasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        NodeBaru->noMhs = nim;
+
+        if (STAR == NULL || nim <= START->Mhs))
+            {
+                if ((START != NULL &&(nim == START->noMhs)) {
+                        cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                        return;
+                    })
+            }
+    }
 }
